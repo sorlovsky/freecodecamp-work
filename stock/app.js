b2ac19f9b4ba34ccbfb41bc499521839872f3e71
sorlovsky <simon.orlovsky@gmail.com>
2017-02-26 21:06:10 -0600

    continued work on stock app

--- stock/app.js
+++ stock/app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var router = express.Router()
+var http = require('http');
 app.set('view engine', 'pug')
 
 function httpGet(theUrl)
@@ -13,14 +14,10 @@ function httpGet(theUrl)
 }
 
 app.get('/', function (req, res) {
-    stockData = httpGet("https://api.iextrading.com/1.0/tops?symbols=AAPL,fb");
-    res.render('index', { title: 'Hey', message: 'Hello there!', data: stockData });
+    res.render('index', { title: 'Hey', message: 'Hello there!', data: stockData[0]});
 })
 
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
-})
-
-
-console.log(httpGet("https://api.iextrading.com/1.0/tops?symbols=AAPL,fb"));
+});
