377dbd2e4619ead9ee8b766cef31d584ca9e457f
sorlovsky <simon.orlovsky@gmail.com>
2017-02-28 22:38:01 -0600

    wiki

--- stock/app.js
+++ stock/app.js
@@ -3,21 +3,28 @@ var app = express()
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var router = express.Router()
 var http = require('http');
+const url = require('url');
+
 app.set('view engine', 'pug')
 
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
+const https = require('https');
+
+var json = https.get('https://api.iextrading.com/1.0/tops?symbols=AAPL', (res) => {
+  res.on('data', (d) => {
+    process.stdout.write(d);
+    console.log(d);
+  });
+
+}).on('error', (e) => {
+  console.error(e);
+});
 
 app.get('/', function (req, res) {
-    res.render('index', { title: 'Hey', message: 'Hello there!', data: stockData[0]});
+    res.render('index', { title: 'Hey', message: 'Hello there!', data: json.length});
 })
 
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+  console.log(url.parse(json));
 });
