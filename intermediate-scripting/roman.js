3981e2192ca989056d9a947a6e0316095b8e13b6
sorlovsky <simon.orlovsky@gmail.com>
2017-03-04 23:40:40 -0600

    roman

--- intermediate-scripting/roman.js
+++ intermediate-scripting/roman.js
@@ -1,1 +1,19 @@
-console.log("hello");
+function convertToRoman(num){
+	var result = ''
+
+	var thousands = num/1000
+	result.concat("M".repeat(thousands))
+	num -= thousands*1000
+
+	var hundreds = num/100
+	result.concat("D".repeat(hundreds))
+	num -= hundreds*100
+
+	var tens = num/10
+	num -= tens*10
+	ones = num
+	
+	return result 
+}
+
+console.log(convertToRoman(15));
