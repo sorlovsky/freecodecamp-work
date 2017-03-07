9b566290fe99ecb19a8b20204f304726a6598a3f
sorlovsky <simon.orlovsky@gmail.com>
2017-03-07 08:58:57 -0600

    finished roman numeral converter

--- intermediate-scripting/roman.js
+++ intermediate-scripting/roman.js
@@ -1,52 +1,59 @@
-function convertToRoman(num){
-	var result = ''
-	var thousands = Math.floor(num/1000);
-	num -= thousands*1000;
-	
-	var hundreds = Math.floor(num/100);
-	num -= hundreds*100;
+function toRoman(num){
+		num = num.toString()
+		var numArr = num.split("");
+		var result = '';
+		for (var i=0; i<numArr.length; i++){
+				numArr[i] = parseInt(numArr[i])*Math.pow(10, numArr.length - i - 1);
 
-	var fifties = Math.floor(num/50);
-	num -= fifties*50;
+				// Less than ten
+				if (numArr[i] < 10){
+					if (numArr[i] < 4){
+						result = result.concat("I".repeat(numArr[i]));
+					} else if (numArr[i] == 4){
+						result = result.concat("IV");
+					} else if (numArr[i] < 9){
+						result = result.concat("V"+"I".repeat(numArr[i]-5));
+					} else {
+						result = result.concat("IX");
+					}
+				}
 
-	var tens = Math.floor(num/10);
-	num -= tens*10;
+				// Less than 100
+				if (numArr[i] < 100){
+					if (numArr[i] < 40){
+						result = result.concat("X".repeat(numArr[i]/10));
+					} else if (numArr[i] >= 40 && numArr[i] < 50){
+						result = result.concat("XL");
+					} else if (numArr[i] < 90){
+						result = result.concat("L" + "X".repeat((numArr[i] - 50)/10));
+					} else {
+						result = result.concat("XC")
+					}
+				}
 
-	var fives = Math.floor(num/5);
-	num -= fives*5;
+				// Less than 1000
+				if (numArr[i] < 1000){
+					if (numArr[i] < 400){
+						result = result.concat("C".repeat(numArr[i]/100));
+					} else if (numArr[i] == 400){
+						result = result.concat("CD");
+					} else if (numArr[i] > 400 && numArr[i] < 900) {
+						result = result.concat("D" + "C".repeat((numArr[i] - 500)/100));
+					} else {
+						result = result.concat("CM");
+					}
 
-	var ones = num;
-	num -= ones
-	
-	// thousands
-	
+				}
 
-	// hundreds
+				if (numArr[i] >= 1000){
+					result = result.concat("M".repeat(numArr[i]/1000));
+				}
+		}
 
-	// tens
-	if (tens >= 1 && tens < 4){
-		result = result.concat("X".repeat(tens));
-	} else if (tens == 4 && fives == 1) {
-		result = result.concat("XL");
-	}
-
- 	// fives	
-	if (fives == 1 && ones == 4){
-		result = result.concat("IX");
-		ones -= 4;
-	} else if (fives == 1){
-		result = result.concat("V");
-	}
-
-	// ones
-	if (ones == 4){
-		result = result.concat("IV");
-	} else {
-		result = result.concat("I".repeat(ones));
-	}
-
-	console.log(thousands, hundreds, tens, fives, ones)
-	return result 
+		return result
 }
 
-console.log(convertToRoman(29));
+console.log(toRoman(1000));
+//for (var i=0; i<10; i++){
+	//console.log(toRoman(100*i), i*100);
+//}
