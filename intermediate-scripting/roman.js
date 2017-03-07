b646528ed0450a9e16ed25813e91758d35ada027
sorlovsky <simon.orlovsky@gmail.com>
2017-03-06 23:22:52 -0600

    roman

--- intermediate-scripting/roman.js
+++ intermediate-scripting/roman.js
@@ -1,19 +1,52 @@
 function convertToRoman(num){
 	var result = ''
-
-	var thousands = num/1000
-	result.concat("M".repeat(thousands))
-	num -= thousands*1000
-
-	var hundreds = num/100
-	result.concat("D".repeat(hundreds))
-	num -= hundreds*100
-
-	var tens = num/10
-	num -= tens*10
-	ones = num
+	var thousands = Math.floor(num/1000);
+	num -= thousands*1000;
 	
+	var hundreds = Math.floor(num/100);
+	num -= hundreds*100;
+
+	var fifties = Math.floor(num/50);
+	num -= fifties*50;
+
+	var tens = Math.floor(num/10);
+	num -= tens*10;
+
+	var fives = Math.floor(num/5);
+	num -= fives*5;
+
+	var ones = num;
+	num -= ones
+	
+	// thousands
+	
+
+	// hundreds
+
+	// tens
+	if (tens >= 1 && tens < 4){
+		result = result.concat("X".repeat(tens));
+	} else if (tens == 4 && fives == 1) {
+		result = result.concat("XL");
+	}
+
+ 	// fives	
+	if (fives == 1 && ones == 4){
+		result = result.concat("IX");
+		ones -= 4;
+	} else if (fives == 1){
+		result = result.concat("V");
+	}
+
+	// ones
+	if (ones == 4){
+		result = result.concat("IV");
+	} else {
+		result = result.concat("I".repeat(ones));
+	}
+
+	console.log(thousands, hundreds, tens, fives, ones)
 	return result 
 }
 
-console.log(convertToRoman(15));
+console.log(convertToRoman(29));
